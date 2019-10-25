c6edc8634545f2cab1980d7a1898309383423ead
giodefelice <giovanni.defelice@cs.ox.ac.uk>
2019-10-25 20:51:51 +0100

    OpenGraph composition

--- graph.py
+++ graph.py
@@ -5,7 +5,7 @@ import networkx as nx
 
 class OpenGraph:
     def __init__(self, dom, cod, graph):
-        assert isinstance(graph, nx.Graph)
+        assert isinstance(graph, nx.MultiGraph)
         assert set(graph.nodes()) == set(range(len(graph.nodes())))
         assert isinstance(dom, int) and isinstance(cod, int)
         assert dom + cod <= len(graph.nodes())
@@ -15,23 +15,23 @@ class OpenGraph:
 
     def then(self, other):
         assert isinstance(other, OpenGraph) and self.cod == other.dom
-        l0 , l1 = len(self.graph.nodes()), len(other.graph.nodes())
-        g0, g1 = self.graph, other.graph
-        g0.remove_nodes_from(range(l1 - self.cod, l1))
+        g0, g1 = self.graph.copy(), other.graph.copy()
+        l0 , l1 = len(g0.nodes()), len(g1.nodes())
+        def relabel(x):
+            return x - other.dom  + l0 - self.cod
+        g0.remove_nodes_from(range(l0 - self.cod, l0))
         g1.remove_nodes_from(range(other.dom))
-        labeling1 = {x : x - other.dom  + l0 - self.cod for x in range(other.dom + 1, l1)}
-        g1 = nx.relabel_nodes(g1, labeling1)
+        g1 = nx.relabel_nodes(g1, {x : relabel(x) for x in g1.nodes()})
         g = nx.union(g0, g1)
         for i in range(self.cod):
-            import pdb; pdb.set_trace()
-            source = self.graph.neighbors(l0 - self.cod + i)[0]
-            target = other.graph.neighbors(i)[0] + l0 - self.cod
+            source = list(self.graph.neighbors(l0 - self.cod + i))[0]
+            target = relabel(list(other.graph.neighbors(i))[0])
             g.add_edge(source, target)
-        return g
+        return OpenGraph(self.dom, other.cod, g)
 
 class Node(OpenGraph):
     def __init__(self, dom, cod):
-        g = nx.Graph([(i, dom) for i in range(dom)])
+        g = nx.MultiGraph([(i, dom) for i in range(dom)])
         g.add_edges_from([(dom, dom + 1 + j) for j in range(cod)])
         super().__init__(dom, cod, g)
 
@@ -52,4 +52,6 @@ d = f.tensor(Wire(x)).then(Wire(y).tensor(g))
 F0 = GraphFunctor({x: 1, y: 2, z: 3, w: 4}, None)
 F = GraphFunctor(F0.ob, {a: Node(F0(a.dom), F0(a.cod)) for a in [f, g, h]})
 
-Node(2,3).then(Node(3,1))
+G = Node(2,3).then(Node(3,1))
+print(G.graph.nodes())
+print(G.graph.edges())
