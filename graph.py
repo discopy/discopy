b624c714232c0178e6b6b3081ceedc633dc694b8
giodefelice <giovanni.defelice@cs.ox.ac.uk>
2019-10-28 14:44:25 +0000

    wip to zx

--- graph.py
+++ graph.py
@@ -1,5 +1,5 @@
 from category import Arrow, Generator, Identity, Functor
-from diagram import Diagram, Box, Wire
+from diagram import Diagram, Box, Wire, MonoidalFunctor
 import pyzx as zx
 import networkx as nx
 
@@ -12,6 +12,10 @@ class OpenGraph:
         assert all(graph.degree(x) == 1 for x in range(dom))
         assert all(graph.degree(len(graph.nodes()) - cod + x) == 1 for x in range(cod))
         self.dom, self.cod, self.graph = dom, cod, graph
+
+    def __repr__(self):
+        return "OpenGraph({}, {}, {}, {})".format(
+            self.dom, self.cod, self.graph.nodes(), self.graph.edges())
 
     def then(self, other):
         assert isinstance(other, OpenGraph) and self.cod == other.dom
@@ -35,7 +39,7 @@ class OpenGraph:
         g0, g1 = self.graph.copy(), other.graph.copy()
         l0 , l1 = len(g0.nodes()), len(g1.nodes())
         dom0, middle0, cod0 = range(self.dom), range(self.dom, l0 - self.cod), range(l0 - self.cod, l0)
-        dom1, middle1, cod1 = range(other.dom), range(other.dom, l1 - self.cod), range(l1 - other.cod, l1)
+        dom1, middle1, cod1 = range(other.dom), range(other.dom, l1 - other.cod), range(l1 - other.cod, l1)
 
         def relabel0(x):
             if x in dom0:
@@ -59,30 +63,57 @@ class OpenGraph:
         g = nx.union(g0, g1)
         return OpenGraph(self.dom + other.dom, self.cod + other.cod, g)
 
+    def to_zx(self):
+        k = zx.Graph()
+        for x in self.graph.nodes(data = True):
+            k.add_vertex(ty = x[1]['type'])
+        k.add_edges(list(self.graph.edges()))
+        return k
 
 class Node(OpenGraph):
-    def __init__(self, dom, cod):
-        g = nx.MultiGraph([(i, dom) for i in range(dom)])
+    def __init__(self, dom, cod, label):
+        g = nx.MultiGraph()
+        g.add_nodes_from(range(dom + 1 +cod), type = 0) #0 is the type of boundary nodes
+        g.add_node(dom, type = label) #the inner node is labeled
+        g.add_edges_from([(i, dom) for i in range(dom)])
         g.add_edges_from([(dom, dom + 1 + j) for j in range(cod)])
         super().__init__(dom, cod, g)
 
+class IdGraph(OpenGraph):
+    def __init__(self, dom):
+        g = nx.MultiGraph()
+        g.add_nodes_from(range(dom + dom), type = 0)
+        g.add_edges_from([(i, dom + i) for i in range(dom)])
+        super().__init__(dom, dom, g)
 
-class GraphFunctor(Functor):
+class GraphFunctor(MonoidalFunctor):
     def __call__(self, d):
         if not isinstance(d,Diagram):
             xs = d if isinstance(d, list) else [d]
             return sum([self.ob[x] for x in xs])
+
+        if isinstance(d, Box):
+            return self.ar[d]
+
         if isinstance(d, Diagram):
-            g = nx.Graph()
+            u = d.dom
+            g = IdGraph(self(u))
+            for f, n in zip(d.nodes, d.offsets):
+                g = g.then(IdGraph(self(u[:n])).tensor(self(f))\
+                     .tensor(IdGraph(self(u[n + len(f.dom):]))))
+                u = u[:n] + f.cod + u[n + len(f.dom):]
             return g
+
 
 x, y, z, w = 'x', 'y', 'z', 'w'
 f, g, h = Box('f', [x], [y, z]), Box('g', [z, x], [w]), Box('h', [y, w], [x])
-d = f.tensor(Wire(x)).then(Wire(y).tensor(g))
+diagram = f.tensor(Wire(x)).then(Wire(y).tensor(g))
 
 F0 = GraphFunctor({x: 1, y: 2, z: 3, w: 4}, None)
-F = GraphFunctor(F0.ob, {a: Node(F0(a.dom), F0(a.cod)) for a in [f, g, h]})
+dict = {a: Node(F0(a.dom), F0(a.cod), '1') for a in [f, g, h]}
+F = GraphFunctor(F0.ob, dict)
 
-G = Node(1,1).tensor(Node(1,3).then(Node(3,1)))
-print(G.graph.nodes())
-print(G.graph.edges())
+print(dict[f].tensor(IdGraph(1)).then(IdGraph(2).tensor(dict[g])))
+print(F(diagram))
+zxdiagram = F(diagram).to_zx()
+print(zxdiagram.edge_set())
