8b37aaf25c536624e56425e5deaff99d48c99bd5
toumix <alexis.toumi@gmail.com>
2019-11-03 12:41:05 +0000

    m

--- cat.py
+++ cat.py
@@ -1,7 +1,7 @@
 from functools import reduce as fold
 
 
-class Object(object):
+class Ob(object):
     def __init__(self, name):
         self._name = name
 
@@ -10,27 +10,30 @@ class Object(object):
         return self._name
 
     def __eq__(self, other):
-        if not isinstance(other, Object):
+        if not isinstance(other, Ob):
             return False
         return self.name == other.name
 
     def __repr__(self):
-        return "Object({})".format(repr(self.name))
+        return "Ob({})".format(repr(self.name))
+
+    def __str__(self):
+        return str(self.name)
 
     def __hash__(self):
         return hash(repr(self))
 
 class Arrow(list):
     def __init__(self, dom, cod, data):
-        assert isinstance(dom, Object)
-        assert isinstance(cod, Object)
+        assert isinstance(dom, Ob)
+        assert isinstance(cod, Ob)
         assert isinstance(data, list)
-        assert all(isinstance(g, Arrow) for g in data)
+        assert all(isinstance(f, Arrow) for f in data)
         u = dom
-        for g in data:
-            assert g.data  # i.e. f is not the identity arrow
-            assert u == g.dom
-            u = g.cod
+        for f in data:
+            assert f.data  # i.e. f is not the identity arrow
+            assert u == f.dom
+            u = f.cod
         assert u == cod
         self._dom, self._cod, self._data = dom, cod, data
         super().__init__(data)
@@ -48,7 +51,11 @@ class Arrow(list):
         return self._data
 
     def __repr__(self):
-        return "Arrow({}, {}, {})".format(self.dom, self.cod, self.data)
+        return "Arrow({}, {}, {})".format(
+            repr(self.dom), repr(self.cod), repr(self.data))
+
+    def __str__(self):
+        return " >> ".join(map(str, self))
 
     def __eq__(self, other):
         if not isinstance(other, Arrow):
@@ -61,15 +68,25 @@ class Arrow(list):
         assert self.cod == other.dom
         return Arrow(self.dom, other.cod, self.data + other.data)
 
+    def __rshift__(self, other):
+        return self.then(other)
+
+    def __lshift__(self, other):
+        return other.then(self)
+
+    def dagger(self):
+        return Arrow(self.cod, self.dom, [f.dagger() for f in self.data[::-1]])
+
     @staticmethod
     def id(x):
-        assert isinstance(x, Object)
+        assert isinstance(x, Ob)
         return Arrow(x, x, [])
 
 class Generator(Arrow):
-    def __init__(self, name, dom, cod):
-        assert isinstance(dom, Object)
-        assert isinstance(cod, Object)
+    def __init__(self, name, dom, cod, dagger=False):
+        assert isinstance(dom, Ob)
+        assert isinstance(cod, Ob)
+        self._dagger = dagger
         self._name, self._dom, self._cod, self._data = name, dom, cod, [self]
         super().__init__(dom, cod, [self])
 
@@ -77,9 +94,16 @@ class Generator(Arrow):
     def name(self):
         return self._name
 
+    def dagger(self):
+        return Generator(self.name, self.cod, self.dom, not self._dagger)
+
     def __repr__(self):
-        return "Generator('{}', {}, {})".format(
-            self.name, self.dom, self.cod)
+        return "Generator(name={}, dom={}, cod={}){}".format(
+            repr(self.name), repr(self.dom), repr(self.cod),
+            ".dagger()" if self._dagger else '')
+
+    def __str__(self):
+        return str(self.name) + (".dagger()" if self._dagger else '')
 
     def __hash__(self):
         return hash(repr(self))
@@ -109,7 +133,7 @@ class Function(Generator):
 
 class Functor:
     def __init__(self, ob, ar):
-        assert all(isinstance(x, Object) for x in ob.keys())
+        assert all(isinstance(x, Ob) for x in ob.keys())
         assert all(isinstance(y, type) for y in ob.values())
         assert all(isinstance(a, Generator) for a in ar.keys())
         assert all(isinstance(b, Function) for b in ar.values())
@@ -123,8 +147,14 @@ class Functor:
     def ar(self):
         return self._ar
 
+    def __eq__(self, other):
+        return self.ob == other.ob and self.ar == other.ar
+
+    def __repr__(self):
+        return "Functor(ob={}, ar={})".format(repr(self.ob), repr(self.ar))
+
     def __call__(self, f):
-        if isinstance(f, Object):
+        if isinstance(f, Ob):
             return self.ob[f]
         if isinstance(f, Generator):
             return self.ar[f]
@@ -133,10 +163,10 @@ class Functor:
         return fold(lambda g, h: g.then(self(h)), f, unit)
 
 
-a, b, c = Object('a'), Object('a'), 'c'
+a, b, c = Ob('a'), Ob('a'), 'c'
 assert a == b and b != c
 
-x, y, z = Object('x'), Object('y'), Object('z')
+x, y, z = Ob('x'), Ob('y'), Ob('z')
 f, g, h = Generator('f', x, y), Generator('g', y, z), Generator('h', z, x)
 assert Arrow.id(x).then(f) == f == f.then(Arrow.id(y))
 assert (f.then(g)).dom == f.dom and (f.then(g)).cod == g.cod
--- circuit.py
+++ circuit.py
@@ -1,12 +1,13 @@
 import pyzx as zx
 import pytket as tk
-from pytket.pyzx import pyzx_to_tk
-from random import random
-from diagram import Diagram, Box, Wire, MonoidalFunctor
+from pytket.pyzx import pyzx_to_tk, tk_to_pyzx
+from gates import GATES_TO_NUMPY
+from moncat import Ty, Diagram, Box, MonoidalFunctor, NumpyFunctor
 
 
-PRO = lambda n: n * [1]
-GATES = tk.OpType.__entries.keys()
+
+#  Turns natural numbers into types encoded in unary.
+PRO = lambda n: sum(n * [Ty(1)], Ty())
 
 
 class Circuit(Diagram):
@@ -16,7 +17,30 @@ class Circuit(Diagram):
 
     def __repr__(self):
         return "Circuit({}, {}, {})".format(
-            len(self.dom), self.nodes, self.offsets)
+            len(self.dom), self.boxes, self.offsets)
+
+    def then(self, other):
+        assert isinstance(other, Circuit)
+        r = super().then(other)
+        return Circuit(len(r.dom), r.boxes, r.offsets)
+
+    def tensor(self, other):
+        assert isinstance(other, Circuit)
+        r = super().tensor(other)
+        return Circuit(len(r.dom), r.boxes, r.offsets)
+
+    @staticmethod
+    def id(n_qubits):
+        if isinstance(n_qubits, Ty):
+            return Circuit(len(n_qubits), [], [])
+        assert isinstance(n_qubits, int)
+        return Circuit(n_qubits, [], [])
+
+    def eval(self):
+        return EVAL(self)
+
+    def to_zx(self):
+        return tk_to_pyzx(self.to_tk()).to_graph()
 
     def to_tk(self):
         c = tk.Circuit(len(self.dom))
@@ -79,21 +103,38 @@ class PytketFunctor(MonoidalFunctor):
             xs = d if isinstance(d, list) else [d]
             return PRO(sum(self.ob[x] for x in xs))
         r = super().__call__(d)
-        return Circuit(len(r.dom), r.nodes, r.offsets)
+        if isinstance(d, Diagram):
+            return Circuit(len(r.dom), r.boxes, r.offsets)
+        return r
 
+#  Gates are unitaries, bras and kets are not. They are only boxes.
+Ket = lambda b: Box('ket' + str(b), PRO(0), PRO(1))
+Bra = lambda b: Box('bra' + str(b), PRO(1), PRO(0))
+Kets = lambda b, n: fold(lambda x, y: x @ y, n * [Ket(b)])
+Bras = lambda b, n: fold(lambda x, y: x @ y, n * [Bra(b)])
+Id = lambda n: Circuit.id(n)
+SWAP, CX = Gate('SWAP', 2), Gate('CX', 2)
+H, S, T = Gate('H', 1), Gate('S', 1), Gate('T', 1)
+X, Y, Z = Gate('X', 1), Gate('Y', 1), Gate('Z', 1)
+Rx = lambda phase: Gate('Rx', 1, phase)
+Rz = lambda phase: Gate('Rz', 1, phase)
 
-c1 = tk.Circuit(3).CX(0, 2).H(1).SWAP(0, 1).Rx(0, 0.25)
-d1 = Circuit.from_tk(c1)
-c2 = d1.to_tk()
-d2 = Circuit.from_tk(c2)
-assert not c1 == c2  # Equality in tket doesn't work!
-assert d1 == d2  # This works as long as there are no interchangers!
+for U in [SWAP, X, Y, Z, S >> S, CX >> CX >> CX]:
+    assert np.all((U >> U).eval() == Circuit.id(U.n_qubits).eval())
+for U in [H, T >> T >> T >> T]:
+    np.allclose((U >> U).eval(), Circuit.id(U.n_qubits).eval())
 
+c1_tk = tk.Circuit(3).SWAP(0, 1).Rx(1, 0.25).CX(1, 2)
+c1 = Circuit.from_tk(c1_tk)
+assert c1 == Circuit(3, [SWAP, Rx(0.25), CX], [0, 1, 1])
+c2_tk = c1.to_tk()
+c2 = Circuit.from_tk(c2_tk)
+assert not c1_tk == c2_tk  # Equality of circuits in tket doesn't work!
+assert c1 == c2  # This works as long as there are no interchangers!
 
-x, y, z, w = 'x', 'y', 'z', 'w'
-f, g, h = Box('f', [x], [y, z]), Box('g', [z, z], [w]), Box('h', [y, w], [x, z])
-d = f.tensor(Wire(z)).then(Wire(y).tensor(g)).then(h)
-F = PytketFunctor({x: 2, y: 1, z: 1, w: 2},
-    {f: Gate('CX', 2), g: Gate('CZ', 2), h: Gate('CCX', 3)})
-assert F(d) == Circuit(3,
-    [Gate('CX', 2), Gate('CZ', 2), Gate('CCX', 3)], [0, 1, 0])
+x, y, z = Ty('x'), Ty('y'), Ty('z')
+f, g, h = Box('f', x, y + z), Box('g', z, y), Box('h', y + z, x)
+d = f @ Diagram.id(z) >> Diagram.id(y) @ g @ Diagram.id(z) >> Diagram.id(y) @ h
+F = CircuitFunctor({x: PRO(2), y: PRO(1), z: PRO(1)},
+                   {f: SWAP, g: Rx(0.25), h: CX})
+assert F(d) == c1
