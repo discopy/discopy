199864ddf3830e0cad68373505e02f0a75e29fe3
toumix <alexis.toumi@gmail.com>
2019-11-03 17:23:15 +0000

    assert only if name is main

--- disco.py
+++ disco.py
@@ -152,7 +152,7 @@ class Word(Grammar, Box):
 class Parse(Grammar):
     def __init__(self, words, cups):
         self._words, self._cups = words, cups
-        self._type = sum((w.cod for w in words), Pregroup())
+        self._type = sum((w.type for w in words), Pregroup())
         dom = sum((w.dom for w in words), Pregroup())
         boxes = words[::-1]  # words are backwards to make offsets easier
         offsets = [len(words) - i - 1 for i in range(len(words))] + cups
@@ -192,23 +192,23 @@ class Model(NumpyFunctor):
             return np.identity(self(d.cod[0]))
         return super().__call__(d)
 
+if __name__ == '__main__':
+    s, n = Pregroup('s'), Pregroup('n')
 
-s, n = Pregroup('s'), Pregroup('n')
+    Alice, Bob = Word('Alice', n), Word('Bob', n)
+    loves = Word('loves', n.r + s + n.l)
+    grammar = Cup(n) @ Wire(s) @ Cup(n.l)
+    sentence = grammar << Alice @ loves @ Bob
+    assert sentence == Parse([Alice, loves, Bob], [0, 1]).interchange(0, 1)\
+                                                         .interchange(1, 2)\
+                                                         .interchange(0, 1)
+    F = Model({s: 1, n: 2},
+              {Alice: [1, 0],
+               loves: [0, 1, 1, 0],
+               Bob: [0, 1]})
+    assert F(sentence) == True
 
-Alice, Bob = Word('Alice', n), Word('Bob', n)
-loves = Word('loves', n.r + s + n.l)
-grammar = Cup(n) @ Wire(s) @ Cup(n.l)
-sentence = grammar << Alice @ loves @ Bob
-assert sentence == Parse([Alice, loves, Bob], [0, 1]).interchange(0, 1)\
-                                                     .interchange(1, 2)\
-                                                     .interchange(0, 1)
-F = Model({s: 1, n: 2},
-          {Alice: [1, 0],
-           loves: [0, 1, 1, 0],
-           Bob: [0, 1]})
-assert F(sentence) == True
-
-snake_l = Cap(n) @ Wire(n) >> Wire(n) @ Cup(n.l)
-snake_r = Wire(n) @ Cap(n.r) >> Cup(n) @ Wire(n)
-assert (F(snake_l) == F(Wire(n))).all()
-assert (F(Wire(n)) == F(snake_r)).all()
+    snake_l = Cap(n) @ Wire(n) >> Wire(n) @ Cup(n.l)
+    snake_r = Wire(n) @ Cap(n.r) >> Cup(n) @ Wire(n)
+    assert (F(snake_l) == F(Wire(n))).all()
+    assert (F(Wire(n)) == F(snake_r)).all()
