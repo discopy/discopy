507971020aad531522e78bb1b6bc16dc11ce6946
toumix <alexis.toumi@gmail.com>
2019-11-08 16:36:30 +0000

    edit init

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='discopy',
       url='https://github.com/toumix/discopy',
       author='Alexis Toumi',
       author_email='alexis.toumi@gmail.com',
-      download_url = 'https://github.com/toumix/discopy/archive/0.0.1.tar.gz',
+      download_url='https://github.com/toumix/discopy/archive/0.0.1-beta.tar.gz',
       install_requires=['numpy'],
       )
