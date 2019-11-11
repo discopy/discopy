6f61a1d52ecba1e56d9add92343a8ce3d32208a9
toumix <alexis.toumi@gmail.com>
2019-11-11 21:21:40 +0000

    edit setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 setup(name='discopy',
-      version='0.0.1-alpha',
+      version='0.0.1',
       package_dir={'discopy': ''},
       packages=['discopy'],
       description='Distributional Compositional Python',
       url='https://github.com/toumix/discopy',
       author='Alexis Toumi',
       author_email='alexis.toumi@gmail.com',
-      download_url='https://github.com/toumix/discopy/archive/0.0.1-alpha.tar.gz',
+      download_url='https://github.com/toumix/discopy/archive/0.0.1.tar.gz',
       install_requires=['numpy'],
       )
