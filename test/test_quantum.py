c68d2945c2506e5b00a459f6c88aeb4cda6c8d83
Nicola Mariella <nicola.mariella@mastercard.com>
2020-12-26 18:53:42 +0000

    fix test_grad CRx grad not impl

--- test/test_quantum.py
+++ test/test_quantum.py
@@ -285,8 +285,6 @@ def test_grad():
     from sympy.abc import phi
     with raises(NotImplementedError):
         Box('f', qubit, qubit, data=phi).grad(phi)
-    with raises(NotImplementedError):
-        CRz(phi).grad(phi)
     assert scalar(1).grad(phi) == Sum([], qubit ** 0, qubit ** 0)
     assert (Rz(phi) + Rz(2 * phi)).grad(phi)\
         == Rz(phi).grad(phi) + Rz(2 * phi).grad(phi)
