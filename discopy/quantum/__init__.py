3a289988bc1901d08940859e56b2642e68c16579
Nicola Mariella <eipiplus1eq0@outlook.ie>
2021-02-03 22:06:48 +0000

    jacobian for circ

--- discopy/quantum/__init__.py
+++ discopy/quantum/__init__.py
@@ -7,7 +7,7 @@ from discopy.quantum.cqmap import C, Q, CQ, CQMap
 from discopy.quantum.circuit import (
     bit, qubit, Circuit, Id, Box, Sum, Swap, CircuitFunctor,
     Discard, MixedState, Measure, Encode, IQPansatz, random_tiling,
-    real_amp_ansatz)
+    real_amp_ansatz, jacobian)
 from discopy.quantum.gates import (
     SWAP, ClassicalGate, QuantumGate, Ket, Bra, Bits, Copy, Match,
     Rx, Ry, Rz, CU1, CRz, CRx, CZ, CX, X, Y, Z, H, S, T, scalar, sqrt, rewire)
