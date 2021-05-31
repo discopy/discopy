8f0ff79bf5a50c0174f772463c31537fd1c27c34
toumix <alexis@toumi.email>
2021-05-31 12:23:11 +0200

    update photonic notebook

--- discopy/__init__.py
+++ discopy/__init__.py
@@ -33,7 +33,7 @@ from discopy.rigid import (
     Ob, Ty, PRO, Box, Diagram, Id, Cup, Cap, Swap, Functor)
 from discopy.tensor import Dim, Tensor, Functor as TensorFunctor
 
-from discopy.quantum import cqmap, zx
+from discopy.quantum import circuit, cqmap, zx, optics
 from discopy.quantum.zx import Z, X, H, SWAP
 from discopy.quantum.cqmap import C, Q, CQMap
 from discopy.quantum.circuit import (
