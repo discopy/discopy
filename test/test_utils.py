6d543e62acaba6df77e43f38756b1b731015da20
Richie Yeung <richie.yeung1@gmail.com>
2021-06-07 23:05:36 +0800

    fix coverage

--- test/test_utils.py
+++ test/test_utils.py
@@ -7,10 +7,10 @@ from discopy.utils import *
 
 zip_mock = MagicMock()
 zip_mock.open().__enter__().read.return_value =\
-    '{"factory": "discopy.cat.Ob", "name": "a"}'
+    '[{"factory": "discopy.cat.Ob", "name": "a"}]'
 
 
 @patch('urllib.request.urlretrieve', return_value=(None, None))
 @patch('zipfile.ZipFile', return_value=zip_mock)
 def test_load_corpus(a, b):
-    assert load_corpus("[fake url]") == Ob("a")
+    assert load_corpus("[fake url]") == [Ob("a")]
