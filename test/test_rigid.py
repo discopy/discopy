fe096da740c94cc1afcc3fbe08c4a2625d74d0a0
y-richie-y <richie.yeung1@gmail.com>
2022-03-15 00:54:48 +0800

    Add cup chaining

--- test/test_rigid.py
+++ test/test_rigid.py
@@ -214,3 +214,22 @@ def test_spider_factory():
                 for k, ob in enumerate(t):
                     assert all(map(ob.__eq__, s.dom[k::len(t)]))
                     assert all(map(ob.__eq__, s.cod[k::len(t)]))
+
+
+def test_cup_chaining():
+    n, s, p = map(Ty, "nsp")
+    A = Box('A', Ty(), n @ p)
+    V = Box('V', Ty(), n.r @ s @ n.l)
+    B = Box('B', Ty(), p.r @ n)
+
+    diagram = (A @ V @ B).cup(1, 5).cup(0, 1).cup(1, 2)
+    expected_diagram = Diagram(
+        dom=Ty(), cod=Ty('s'),
+        boxes=[
+            A, V, B, Swap(p, n.r), Swap(p, s), Swap(p, n.l), Cup(p, p.r),
+            Cup(n, n.r), Cup(n.l, n)],
+        offsets=[0, 2, 5, 1, 2, 3, 4, 0, 1])
+    assert diagram == expected_diagram
+
+    with raises(ValueError):
+        Id(n @ n.r).cup(0, 2)
