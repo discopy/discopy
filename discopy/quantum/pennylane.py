ed540c989ef7f90413af88000297cbd74a6dd0c5
Charles London <charles.london@cambridgequantum.com>
2022-06-21 13:47:36 +0100

    more efficient calculation of valid states

--- discopy/quantum/pennylane.py
+++ discopy/quantum/pennylane.py
@@ -35,6 +35,10 @@ OP_MAP = {
 
 
 def tk_op_to_pennylane(tk_op, str_map):
+    """
+    Extract the operation, parameters and wires from
+    a tket Op, and return the corresponding Pennylane op
+    """
     wires = [x.index[0] for x in tk_op.qubits]
     params = tk_op.op.params
 
@@ -53,6 +57,11 @@ def tk_op_to_pennylane(tk_op, str_map):
 
 
 def extract_ops_from_tk(tk_circ: Circuit, str_map):
+    """
+    Extract the operations, and corresponding parameters and wires
+    from a pytket Circuit. Returns these as lists to use in
+    constructing Pennylane circuit.
+    """
     op_list, params_list, wires_list = [], [], []
 
     for op in tk_circ.__iter__():
@@ -66,7 +75,7 @@ def extract_ops_from_tk(tk_circ: Circuit, str_map):
 
 
 def get_post_selection_dict(tk_circ):
-    """Return post selections based on qubit indices."""
+    """Return post-selections based on qubit indices."""
     q_post_sels = {}
     for q, c in tk_circ.qubit_to_bit_map.items():
         q_post_sels[q.index[0]] = tk_circ.post_selection[c.index[0]]
@@ -74,6 +83,12 @@ def get_post_selection_dict(tk_circ):
 
 
 def to_pennylane(disco_circuit: Circuit, probabilities=False):
+    """
+    Return a PennylaneCircuit equivalent to the input DisCoPy
+    circuit. `probabilties` determines whether the PennylaneCircuit
+    returns states (as in DisCoPy), or probabilties (to be more
+    compatible with automatic differentiation in Pennylane)
+    """
     symbols = disco_circuit.free_symbols
     str_map = {str(s): s for s in symbols}
 
@@ -100,6 +115,10 @@ def to_pennylane(disco_circuit: Circuit, probabilities=False):
 
 
 class PennylaneCircuit:
+    """
+    Implements a pennylane circuit with post-selection
+    """
+
     def __init__(self, ops, params, wires, probabilities,
                  post_selection, scale, n_qubits, device):
         self.ops = ops
@@ -107,12 +126,30 @@ class PennylaneCircuit:
         self._contains_sympy = self.contains_sympy()
         self.wires = wires
         self.probs = probabilities
-        self.post_selection = post_selection
+        self._post_selection = post_selection
         self.scale = scale
         self.n_qubits = n_qubits
         self.device = device
+        self._valid_states = self.get_valid_states()
+
+    @property
+    def post_selection(self):
+        return self._post_selection
+
+    @post_selection.setter
+    def post_selection(self, post_selection_dict):
+        self._post_selection = post_selection_dict
+        self._valid_states = self.get_valid_states()
+
+    @property
+    def valid_states(self):
+        return self._valid_states
 
     def contains_sympy(self):
+        """
+        Determine if the circuit parameters are
+        concrete or contain Sympy symbols
+        """
         for expr_list in self.params:
             if any(isinstance(expr, sympy.Expr) for
                    expr in expr_list):
@@ -120,6 +157,10 @@ class PennylaneCircuit:
         return False
 
     def draw(self, symbols=None, weights=None):
+        """
+        Prints a string representation of the circuit
+        similar to `qml.draw`, but including post-selection
+        """
         if self._contains_sympy:
             params = self.param_substitution(symbols, weights)
         else:
@@ -127,16 +168,20 @@ class PennylaneCircuit:
                       for p in self.params]
 
         wires = qml.draw(self.make_circuit())(params).split("\n")
-        for k, v in self.post_selection.items():
+        for k, v in self._post_selection.items():
             wires[k] = wires[k].split("┤")[0] + "┤" + str(v) + ">"
 
         print("\n".join(wires))
 
     def get_valid_states(self):
+        """
+        Determines which of the output states of the circuit are
+        compatible with the post-selections
+        """
         keep_indices = []
-        fixed = ['0' if self.post_selection.get(i, 0) == 0 else
+        fixed = ['0' if self._post_selection.get(i, 0) == 0 else
                  '1' for i in range(self.n_qubits)]
-        open_wires = set(range(self.n_qubits)) - self.post_selection.keys()
+        open_wires = set(range(self.n_qubits)) - self._post_selection.keys()
         permutations = [''.join(s) for s in product('01',
                                                     repeat=len(open_wires))]
         for perm in permutations:
@@ -147,7 +192,10 @@ class PennylaneCircuit:
         return keep_indices
 
     def make_circuit(self):
-
+        """
+        Construct the qml.QNode, a circuit that can be used with
+        autograd to construct hybrid models
+        """
         @qml.qnode(self.device, interface="torch")
         def circuit(circ_params):
             for op, params, wires in zip(self.ops, circ_params, self.wires):
@@ -161,12 +209,14 @@ class PennylaneCircuit:
         return circuit
 
     def post_selected_circuit(self, params):
+        """
+        Runs the circuit with the given parameters and returns
+        the post-selected output
+        """
         states = self.make_circuit()(params)
 
-        open_wires = self.n_qubits - len(self.post_selection)
-        valid_states = self.get_valid_states()
-
-        post_selected_states = states[list(valid_states)]
+        open_wires = self.n_qubits - len(self._post_selection)
+        post_selected_states = states[list(self._valid_states)]
 
         if self.probs:
             post_selected_states = \
@@ -177,6 +227,9 @@ class PennylaneCircuit:
         return torch.reshape(post_selected_states, (2,) * open_wires)
 
     def param_substitution(self, symbols, weights):
+        """
+        Substitutes Sympy symbols for concrete parameters
+        """
         concrete_params = []
         for expr_list in self.params:
             concrete_list = []
@@ -191,6 +244,11 @@ class PennylaneCircuit:
                 for p in concrete_params]
 
     def eval(self, symbols=None, weights=None):
+        """
+        Evaluates the circuit. The symbols should be those
+        from the original DisCoPy diagram, which will be substituted
+        for the concrete parameters in weights.
+        """
         if self._contains_sympy:
             concrete_params = self.param_substitution(symbols, weights)
             return self.post_selected_circuit(concrete_params)
