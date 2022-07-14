b72f4df46788c6f866b2de28dc555186c3d7ab90
y-richie-y <richie.yeung1@gmail.com>
2022-07-14 17:37:40 +0100

    More optics coverage

--- test/test_optics.py
+++ test/test_optics.py
@@ -1,4 +1,10 @@
-from discopy.quantum.optics import Comonoid, Counit, Endo, Monoid, Unit, to_matrix, Id, PRO
+from pytest import raises
+
+import numpy as np
+
+from discopy.quantum.optics import (ar_to_path, BBS, Box, Comonoid, Counit, Endo, Monoid,
+                                    Unit, to_matrix, to_path, Id, MZI, PathBox,
+                                    PRO, TBS)
 from discopy.monoidal import Swap
 
 unit = Unit()
@@ -47,3 +53,43 @@ def test_homomorphism():
 
     check(comonoid >> x @ y >> monoid, Endo(0.123 + 0.321))
     check(x >> y, Endo(0.123 * 0.321))
+
+
+def test_bad_queries():
+    network = MZI(0.2, 0.4) @ MZI(0.2, 0.4) >> Id(1) @ MZI(0.2, 0.4) @ Id(1)
+    with raises(ValueError):
+        network.amp([0, 1], [2, 3])
+    with raises(ValueError):
+        network.pdist_prob([0, 1], [2, 3], np.array([[1, 0.1], [0.1, 1]]))
+    with raises(ValueError):
+        network.dist_prob([0, 1], [2, 3], np.array([[1, 0.1], [0.1, 1]]))
+
+
+def test_bad_box():
+    with raises(TypeError):
+        Box('use PRO', 1, PRO(2))
+    with raises(TypeError):
+        Box('use PRO', PRO(1), 2)
+
+
+def test_box_repr():
+    box1 = Box('repr box', PRO(1), PRO(2))
+    assert repr(box1) == "optics.Box('repr box', PRO(1), PRO(2))"
+
+    box2 = PathBox('repr path box', PRO(1), PRO(2))
+    assert repr(box2) == "optics.PathBox('repr path box', PRO(1), PRO(2))"
+
+
+def test_to_matrix():
+    mzi = MZI(0.18, 0.97)
+    bbs1 = BBS(0.123)
+    tbs = TBS(0.321)
+    bbs2 = BBS(0)
+    network = mzi >> bbs1 >> tbs >> bbs2
+    assert to_matrix(to_path(network)) == to_matrix(network)
+    assert to_matrix(to_path(network).dagger()) == to_matrix(network).dagger()
+
+    assert np.allclose((mzi >> mzi.dagger()).array, np.eye(2))
+
+    with raises(NotImplementedError):
+        ar_to_path(123)
