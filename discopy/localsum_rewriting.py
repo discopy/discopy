8c157074df81d34360a2a6ee38ee19915da102f3
Gina Muuss <muuss@uni-bonn.de>
2022-08-29 15:49:41 +0200

    distribute over composition for monoidal diagrams

--- discopy/localsum_rewriting.py
+++ discopy/localsum_rewriting.py
@@ -1,3 +1,4 @@
+from typing import List
 import discopy.cat as cat
 import discopy.monoidal as monoidal
 
@@ -185,4 +186,117 @@ def distribute_tensor(
     return monoidal.Diagram(diagram.dom, diagram.cod, new_terms, new_offsets)
 
 
+def _check_nothing_in_way(diagram: monoidal.Diagram, index_start: int, index_end: int, offsets_to_check: List[int]) -> bool:
+    """
+    Checks whether there are only identities at the given offesets starting at index_start up to index_end (both excluded)
+    Will return true if there are only identities, otherwise false.
 
+    Parameters
+    ----------
+    diagram: Diagram
+        The diagram to perform the check on
+    index_sum: int
+        The index to start from (exclusive)
+    index_distribute: int
+        The index to end on (exclusive)
+    offsets_to_check: List[int]
+        The offsets to check
+    """
+    if index_start > index_end:
+        index_start, index_end = index_end, index_start
+    for i in range(index_start + 1, index_end):
+        if diagram.offsets[i] in offsets_to_check:
+            return False
+        if diagram.offsets[i] + len(diagram.boxes[i].dom.objects) in offsets_to_check:
+            return False
+        if diagram.offsets[i] + len(diagram.boxes[i].cod.objects) in offsets_to_check:
+            return False
+    return True
+        
+
+
+def distribute_composition_monoidal(
+    diagram: monoidal.Diagram,
+    index_sum: int,
+    index_distribute: int,
+    arrow_construction=lambda dom, cod, boxes: monoidal.Diagram(dom, cod, boxes),
+) -> monoidal.Diagram:
+    """
+    Distributes the sum at index_sum with the box at index_distribute (if possible)
+
+    Parameters
+    ----------
+    diagram: Diagram
+        The diagram to perform the operation on
+    index_sum: int
+        The index of the sum to distribute for
+    index_distribute: int
+        the index of the term to distribute
+    arrow_construction: callable
+        callable returning the arrow equivalent in the relevant category
+
+    Raises
+    ------
+      IndexError: The index of sum does not correspond to a box
+                  or distribute_up_to is negative or to large
+      TypeError: The box at index_sum does not have type sum
+    """
+    if len(diagram.boxes) <= index_sum or index_sum < 0:
+        raise IndexError("index to large or negative, no such box")
+    if (
+        len(diagram.boxes) <= index_distribute
+        or index_distribute < 0
+        or index_distribute == index_sum
+    ):
+        raise IndexError(
+            "distribute_up_to to large, negative or \
+equal to index_sum, no such box"
+        )
+    if not isinstance(diagram.boxes[index_sum], cat.LocalSum):
+        raise TypeError("box at index %d is not a LocalSum", index_sum)
+
+    layers_index = list(
+        range(
+            diagram.offsets[index_sum],
+            diagram.offsets[index_sum] + len(diagram.boxes[index_sum].dom.objects),
+        )
+    )
+    layers_partner = list(
+        range(
+            diagram.offsets[index_distribute],
+            diagram.offsets[index_distribute]
+            + len(diagram.boxes[index_distribute].dom.objects),
+        )
+    )
+
+    if min(layers_partner) < min(layers_index):
+        raise IndexError(f"Cannot distribute, box to distirbute at layer {index_distribute} starts before sum")
+
+    if max(layers_partner) > max(layers_index):
+        raise IndexError(f"Cannot distribute, box to distirbute at layer {index_distribute} ends after sum")
+
+    if not _check_nothing_in_way(diagram, index_sum, index_distribute, layers_partner):
+        raise IndexError(f"something is in the way, cannot directly distribute")
+    
+
+    if index_sum < index_distribute:
+        # distribute to the right
+        terms = [
+            g.then(
+                diagram.boxes[index_distribute]
+            )
+            for g in diagram.boxes[index_sum].terms
+        ]
+    else:
+        # distribute to the left
+        terms = [
+            diagram.boxes[index_distribute].then(g)
+            for g in diagram.boxes[index_sum].terms
+        ]
+    term = monoidal.LocalSum(terms)
+    
+    new_offsets = diagram.offsets[:index_distribute] + diagram.offsets[index_distribute + 1:]
+    new_boxes = diagram.boxes
+    new_boxes[index_sum] = term
+    new_boxes = new_boxes[:index_distribute] + new_boxes[index_distribute + 1:]
+    return monoidal.Diagram(diagram.dom, diagram.cod, new_boxes, new_offsets)
--- test/test_localsum.py
+++ test/test_localsum.py
@@ -1,26 +1,28 @@
 # -*- coding: utf-8 -*-
 
 from pytest import raises, fixture
-from discopy.cat import *
-from discopy.localsum_rewriting import *
+import discopy
+import discopy.cat as cat
+import discopy.monoidal as monoidal
+from discopy.localsum_rewriting import distribute_composition_cat, distribute_composition_monoidal, distribute_tensor, _check_nothing_in_way
 
 
 @fixture
 def localsum_testdata_cat():
-    x, y, z = Ob('x'), Ob('y'), Ob('z')
-    f = Box('f', x, y)
-    g = Box('g', x, y)
-    h = Box('h', y, z)
-    fg = LocalSum([f, g])
+    x, y, z = cat.Ob('x'),cat.Ob('y'),cat.Ob('z')
+    f = cat.Box('f', x, y)
+    g = cat.Box('g', x, y)
+    h = cat.Box('h', y, z)
+    fg = cat.LocalSum([f, g])
     k = fg >> h
-    l = Box("h'", z, x) >> fg
+    l = cat.Box("h'", z, x) >> fg
     return x, y, z, f, g, h, fg, k, l
 
 
 def test_localsum_creation(localsum_testdata_cat):
     _, _, _, _, _, _, _, k, l = localsum_testdata_cat
-    assert isinstance(k.boxes[0], LocalSum)
-    assert isinstance(l.boxes[1], LocalSum)
+    assert isinstance(k.boxes[0], cat.LocalSum)
+    assert isinstance(l.boxes[1], cat.LocalSum)
 
 
 def test_distribute_composition_cat_left(localsum_testdata_cat):
@@ -51,15 +53,15 @@ def test_distribute_composition_cat_right(localsum_testdata_cat):
 
 def test_distribute_composition_cat_right2(localsum_testdata_cat):
     x, y, z, _, _, _, _, _, _ = localsum_testdata_cat
-    diag = Box("a", x, y) >> Box("b", y, x) >> LocalSum([Box("c", x, z), Box(
-        "d", x, z)]) >> Box("e", z, y) >> Box("f", y, y) >> Box("g", y, x)
+    diag = cat.Box("a", x, y) >> cat.Box("b", y, x) >> cat.LocalSum([cat.Box("c", x, z), cat.Box(
+        "d", x, z)]) >> cat.Box("e", z, y) >> cat.Box("f", y, y) >> cat.Box("g", y, x)
     a = distribute_composition_cat(diag, 2, 1)
     assert len(a.boxes) == 5
-    assert isinstance(a.boxes[1], LocalSum)
+    assert isinstance(a.boxes[1], cat.LocalSum)
     assert a.boxes[1].terms[0].boxes[0].name == "b"
     a = distribute_composition_cat(diag, 2, 4)
     assert len(a.boxes) == 4
-    assert isinstance(a.boxes[2], LocalSum)
+    assert isinstance(a.boxes[2], cat.LocalSum)
     assert len(a.boxes[2].terms[0].boxes) == 3
     assert a.boxes[2].terms[0].boxes[0].name == "c"
     assert a.boxes[2].terms[0].boxes[1].name == "e"
@@ -97,3 +99,47 @@ def test_distribute_tensor(localsum_testdata_monoidal):
     assert len(diag.boxes[1].terms[1].layers.boxes) == 2
     assert diag.boxes[1].terms[0].offsets == [0,1]
     assert diag.boxes[1].terms[1].offsets == [0,1]
+
+def test_check_nothing_in_way():
+    x, y, z = monoidal.Ty('x'), monoidal.Ty('y'), monoidal.Ty('z')
+    f = monoidal.Box('f', x, y)
+    g = monoidal.Box('g', x, y)
+    h = monoidal.Box('h', y, z)
+    diag = monoidal.LocalSum([f, g]) @ h
+    diag = diag >> (monoidal.Box("t", y, x) @ monoidal.Box("t2", z, x)) >> monoidal.Box('f', x @ x, y)
+    assert _check_nothing_in_way(diag, 0, 2, [0])
+    assert not _check_nothing_in_way(diag, 0, 3, [0])
+
+
+def test_distribute_composition_monoidal_right():
+    x, y, z = monoidal.Ty('x'), monoidal.Ty('y'), monoidal.Ty('z')
+    f = monoidal.Box('f', x, y)
+    g = monoidal.Box('g', x, y)
+    h = monoidal.Box('h', y, z)
+    diag = monoidal.LocalSum([f, g]) @ h
+    diag = diag >> (monoidal.Box("t", y, x) @ monoidal.Box("t2", z, x)) >> monoidal.Box('f', x @ x, y)
+    a = str(diag)
+    diag = distribute_composition_monoidal(diag, 0, 2)
+    b = str(diag)
+    assert len(diag.boxes) == 4
+    assert len(diag.boxes[0].terms) == 2
+    assert len(diag.boxes[0].terms[0].boxes) == 2
+    assert diag.boxes[0].terms[0].boxes[0].name == "f"
+    assert diag.boxes[0].terms[0].boxes[1].name == "t"
+    assert len(diag.boxes[0].terms[1].boxes) == 2
+    assert diag.boxes[0].terms[1].boxes[0].name == "g"
+    assert diag.boxes[0].terms[1].boxes[1].name == "t"
+
+
+def test_distribute_composition_monoidal_left():
+    x, y, z = monoidal.Ty('x'), monoidal.Ty('y'), monoidal.Ty('z')
+    diag = monoidal.Box("a", x, y) >> monoidal.Box("b", y, x) >> monoidal.LocalSum([monoidal.Box("c", x, z), monoidal.Box("d", x, z)]) >> monoidal.Box("e", z, y)>> monoidal.Box("f", y, y)>> monoidal.Box("g", y, x)
+    diag = distribute_composition_monoidal(diag, 2, 1)
+    assert len(diag.boxes) == 5
+    assert len(diag.boxes[1].terms) == 2
+    assert len(diag.boxes[1].terms[0].boxes) == 2
+    assert diag.boxes[1].terms[0].boxes[0].name == "b"
+    assert diag.boxes[1].terms[0].boxes[1].name == "c"
+    assert len(diag.boxes[1].terms[1].boxes) == 2
+    assert diag.boxes[1].terms[1].boxes[0].name == "b"
+    assert diag.boxes[1].terms[1].boxes[1].name == "d"
