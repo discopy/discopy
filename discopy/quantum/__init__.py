a31263fc679abdbe9916aa71e5a4aabdb45a70da
nikhilkhatri <nikhilkhatri97@gmail.com>
2022-11-11 10:40:54 +0000

    Add ansatz 8 from arXiv:1905.10876

--- discopy/quantum/__init__.py
+++ discopy/quantum/__init__.py
@@ -8,7 +8,7 @@ from discopy.quantum.circuit import (
     bit, qubit, Digit, Qudit, Circuit, Id, Box, Sum, Swap,
     Functor as CircuitFunctor,
     Discard, MixedState, Measure, Encode, IQPansatz, Sim14ansatz, Sim15ansatz,
-    random_tiling, real_amp_ansatz)
+    Sim8ansatz, random_tiling, real_amp_ansatz)
 from discopy.quantum.gates import (
     SWAP, ClassicalGate, QuantumGate, Controlled, Ket, Bra, Bits, Copy, Match,
     Rx, Ry, Rz, CU1, CRz, CRx, CZ, CX, X, Y, Z, H, S, T, scalar, sqrt, rewire)
