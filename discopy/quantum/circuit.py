7dedcc325e46fef6d79ac26e5bcbc34f812caead
Charlie London <36036324+le-big-mac@users.noreply.github.com>
2023-03-07 11:39:39 +0000

    Improve PennyLane backend. (#127)
    Allow different devices and differentiation methods, and improve PennyLane circuit speed.
    
    Co-authored-by: Richie Yeung <richie.yeung1@gmail.com>

--- discopy/quantum/circuit.py
+++ discopy/quantum/circuit.py
@@ -564,7 +564,8 @@ class Circuit(tensor.Diagram):
         from discopy.quantum.tk import to_tk
         return to_tk(self)
 
-    def to_pennylane(self, probabilities=False):
+    def to_pennylane(self, probabilities=False, backend_config=None,
+                     diff_method='best'):
         """
         Export DisCoPy circuit to PennylaneCircuit.
 
@@ -581,7 +582,9 @@ class Circuit(tensor.Diagram):
         :class:`discopy.quantum.pennylane.PennylaneCircuit`
         """
         from discopy.quantum.pennylane import to_pennylane
-        return to_pennylane(self, probabilities=probabilities)
+        return to_pennylane(self, probabilities=probabilities,
+                            backend_config=backend_config,
+                            diff_method=diff_method)
 
     @staticmethod
     def from_tk(*tk_circuits):
