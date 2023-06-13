7f72289ba9e73b42d099193c6ab2996231f37780
Boldi <boldar99@gmail.com>
2023-06-13 15:13:09 +0200

    Fix coverage

--- test/test_quantum.py
+++ test/test_quantum.py
@@ -929,4 +929,5 @@ def test_CCX_decompose(x, y, z):
 
 def test_loads_dumps():
     from discopy.utils import loads, dumps
+    assert loads(dumps(Qudit(3))) == Qudit(3)
     assert loads(dumps(Rx(1))) == Rx(1)
