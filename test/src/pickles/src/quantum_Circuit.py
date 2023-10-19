335561d2e1a94a81d481bf090a81818993937f6d
Boldi <boldar99@gmail.com>
2023-10-19 16:18:48 +0100

    Updating 0.6 pickles

--- test/src/pickles/src/quantum_Circuit.py
+++ test/src/pickles/src/quantum_Circuit.py
@@ -11,4 +11,9 @@ boxes = [
     Rx(0.446), Rz(0.256), Rx(z), CX, H, sqrt(2), Bra(0, 0)]
 offsets = [
     0, 0, 0, 0, 0, 0, 1, 2, 0, 1, 2, 2, 3, 2, 0, 0, 0, 0, 0, 0, 1, 0]
+
+# 0.6
+# pick = Circuit(dom=qubit ** 0, cod=qubit, boxes=boxes, offsets=offsets)
+
+# 1.1
 pick = Circuit.decode(Ty(), zip(boxes, offsets))
