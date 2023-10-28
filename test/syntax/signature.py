d71540a633f5dbb0fd8277e0768a558b4cf43f6f
Martin Coll <mcoll@dc.uba.ar>
2023-10-28 10:42:12 -0300

    signature as arrow and box

--- test/syntax/signature.py
+++ test/syntax/signature.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import networkx as nx
-import matplotlib.pyplot as plt
-from discopy import cat, monoidal, signature
-from discopy.utils import AxiomError
+from discopy import signature
 
 
-def test_Arrow_str():
+def test_Arrow():
     sig = nx.DiGraph()
+    sig.add_edge('x', 'x')
     sig.add_edge('w', 'f')
     sig.add_edge('x', 'f')
     sig.add_edge('f', 'y')
     sig.add_edge('y', 'g')
     sig.add_edge('g', 'z')
 
+    ar = signature.Arrow(sig)
     f = signature.Box(sig, 'f')
-    g = signature.Box(sig, 'g')
-    (f >> g).draw()
+    x = signature.Box(sig, 'x')
+    (f @ ar @ x).draw()
