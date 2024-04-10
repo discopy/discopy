99fd796cce56e0f74621732dcb41069e6164b69c
toumix <alexis@toumi.email>
2024-04-10 17:56:18 +0200

    random walk passes

--- test/syntax/feedback.py
+++ test/syntax/feedback.py
@@ -1,4 +1,5 @@
 from pytest import raises
+from random import choice, seed
 
 from discopy import *
 from discopy.feedback import *
@@ -22,20 +23,32 @@ def test_Diagram_repr():
     assert eval(repr(fib)) == fib
 
 
-def test_fibonacci():
-    x = Ty('int')
-    zero, one, plus = Box('0', Ty(), x), Box('1', Ty(), x), Box('+', x @ x, x)
-    fib = ((Copy(x) >> one.head @ Diagram.wait(x) @ x
-            >> FollowedBy(x) @ x >> plus).delay()
-            >> zero.head @ x.delay() >> FollowedBy(x) >> Copy(x)).feedback()
+def test_functor_python_stream():
+    x = Ty('x')
+    zero, wait = Box('zero', Ty(), x), Diagram.wait(x)
+    cod = stream.Category(python.Ty, python.Function)
+    F = Functor({x: int}, {zero: lambda: 0}, cod)
+    assert F(wait @ zero).unroll(2).now(1, 2, 3) == (0, 1, 0, 2, 0, 3)
+
+
+def test_walk():
+    seed(420)
+    X, fby = Ty('X'), FollowedBy(Ty('X'))
+    zero, rand, plus = Box('0', Ty(), X), Box('rand', Ty(), X), Box('+', X @ X, X)
+
+    @Diagram.feedback
+    @Diagram.from_callable(X.d, X @ X)
+    def walk(x):
+        y = fby(zero.head(), plus.d(rand.d(), x))
+        return (y, y)
 
     F = Functor(
-        ob={x: int},
+        ob={X: int},
         ar={zero: lambda: 0,
-            one: lambda: 1,
+            rand: lambda: choice([-1, +1]),
             plus: lambda x, y: x + y},
         cod=stream.Category(python.Ty, python.Function))
 
-    F(Diagram.wait(x))
-    assert F(zero >> Diagram.wait(x)).unroll(5).now() == ((), 0, 0, 0, 0)
-    assert F(fib).unroll(5).now() == (0, 1, 1, 2, 3)
+    assert F(walk).unroll(10).now() == (0, -1, 0, 1, 2, 1, 0, -1, 0, 1)
+    assert F(walk).unroll(10).now() == (0, -1, 0, 1, 2, 1, 2, 3, 2, 1)
+    assert F(walk).unroll(10).now() == (0, 1, 0, 1, 0, -1, 0, -1, 0, -1)
