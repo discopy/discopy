3ed6b737961f46765793a06033b6e23cf80b50d6
toumix <alexis@toumi.email>
2024-11-28 13:04:26 +0100

    fix convergence

--- test/semantics/python.py
+++ test/semantics/python.py
@@ -54,20 +54,19 @@ def test_additive_Function():
         return 3 * m + 1 if m % 2 else m // 2, 0 if n == 1 and m == 2 else 1
 
     f = Function(f_inside, X + X, X + X)
+    g = Function(lambda m: m // 2, X, X)
 
     # This converges if https://en.wikipedia.org/wiki/Collatz_conjecture holds.
     assert f.trace()(42) == 1 and xs == [42, 21, 64, 32, 16, 8, 4, 2]
 
-    g = Function(lambda m: m + 1, X, X)
-
     eq = lambda *fs: all(fs[0].is_parallel(f) for f in fs) and all(
         len(set(f(42, i) for f in fs)) == 1 for i in range(len(fs[0].dom)))
-    
+
     assert eq(Swap(X, X) >> m, m)
     assert eq(X @ e >> m, Id(X), e @ X >> m)
     assert eq(m @ X >> m, X @ m >> m, Function.merge(X, n=3))
     assert eq(Function.merge(X + X), X @ Swap(X, X) @ X >> m @ m)
-    
+
     assert eq(Swap(X, X).trace(), Id(X))  # Yanking
     assert eq((f >> X @ g).trace(), (X @ g >> f).trace())  # Sliding
     assert eq((g @ X >> f).trace(), g >> f.trace())  # Left-naturality
